481cbe741598d943856a7a73c487eec0e2da73eb
Kensw0 <mehdibergame@gmail.com>
2026-06-04 18:53:38 +0100

    Delete FULL IPIRNET PROJECT12 directory

--- FULL IPIRNET PROJECT12/gestion_des_stagiaires/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare(strict_types=1);
-require __DIR__ . '/includes/bootstrap.php';
-
-unset($_SESSION['gds_admin']);
-session_regenerate_id(true);
-flash_set('Déconnexion effectuée.');
-redirect('login.php');
