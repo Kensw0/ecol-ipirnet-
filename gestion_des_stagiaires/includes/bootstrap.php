7120761be1ae0d3967b12f51b227402fa0572464
Kensw0 <mehdibergame@gmail.com>
2026-06-04 13:48:10 +0100

    Delete gestion_des_stagiaires directory

--- gestion_des_stagiaires/includes/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
-require __DIR__ . '/db.php';
-require __DIR__ . '/helpers.php';
-require __DIR__ . '/auth.php';
-gds_require_admin_session();
